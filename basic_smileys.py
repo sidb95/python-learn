04c6635f3b8c557ff93434054fe98986ef3f39de
Sidddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-04 19:17:06 +0530

    Update: 1.0

---
+++ basic_smileys.py
@@ -0,0 +1,16 @@
+/*
+sidb95
+sbhatore95@gmail.com
+basic_smileys
+*/
+
+def basic_smileys(void):
+  hairs = ['l', '1', '|']
+# three kinds of haircuts
+  eyes = ['B', ':', ';']
+# three kinds of eyes
+  noses = ['-', '--', '+', '++']
+# four kinds of noses
+  mouths = ['$', '#', '@', 'π']
+# four kinds of mouths
+  
