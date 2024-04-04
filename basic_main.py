61cdd6077ce93c69aa057a6e76f1eafd0ef2b6b5
Sidddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-04 18:47:54 +0530

    Update: percentages, solution file.

---
+++ basic_main.py
@@ -0,0 +1,15 @@
+/*
+sidb95
+basic_main
+sbhatore95@gmail.com
+04.04.2024
+*/
+
+def firstHundredNnumbers(void) {
+  for i in range(1..101):
+      print(i)
+      print("%\n")
+  return
+
+
+        
