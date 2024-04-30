f1c3ad3efb1115e70e2fcd6fe214f9d46d79bc9f
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-30 06:50:22 +0530

    Update class_one.py

--- core/class-variables/class_one.py
+++ core/class-variables/class_one.py
@@ -1,3 +1,24 @@
 # class_one
 # bhatoresiddharth@gmail.com 
-# 
+# sidb95
+# 30th Apr 2024
+
+# Solution for various hello world programs.
+
+class Solution:
+    def __init__(self, n, s="hi Prithvi"):
+      self.n = n
+      self.s = strVar
+      return self
+
+    def greetingsHouse(void):
+      print(s.strVar)
+      print('\n')
+
+# main file
+
+def __main__(Solution):
+  myHiSol = Solution(10, "hi Vayu")
+  myHiSol.greetingsHouse(1)
+
+  
